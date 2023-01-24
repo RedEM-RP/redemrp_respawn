0b3bbc242e116948bb6ed5fad2b5f05b6ecf8258
Quin Schurman <schurmanq@gmail.com>
2023-01-24 14:26:29 -0500

    2023.01 Release

--- html/js/listener.js
+++ html/js/listener.js
@@ -1,7 +1,7 @@
 $(function(){
 	window.onload = (e) => {
 		window.addEventListener('message', (event) => {
-				if (event.data.type === 1){
+			if (event.data.type === 1){
 				showMap(event.data.showMap)
 			}
 		});
